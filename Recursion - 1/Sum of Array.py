90d118a63a8d764e05e21c42584776fdb6107193
Akshatt Dahiya <90125966+akshattdahiya@users.noreply.github.com>
2023-06-22 09:48:38 +0530

    Create Sum of Array.py

---
+++ Recursion - 1/Sum of Array.py
@@ -0,0 +1,11 @@
+def sumArray(arr): 
+  n=len(arr) 
+  if(n == 1): 
+    return arr[0] 
+  smallAns = sumArray(arr[:n-1]) 
+  return smallAns + arr[n-1] 
+from sys import setrecursionlimit 
+setrecursionlimit(11000) 
+n=int(input()) 
+arr=list(int(i) for i in input().strip().split(' ')) 
+print(sumArray(arr))
